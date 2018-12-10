a2b168ca516f214c18de5c2d650362592606d728
unknown <Invitados@GBENJUMEAV.pslcol.com.co>
2018-12-10 14:51:00 -0500

    Value Name

--- src/main/java/com/example/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/example/demo/helloworld/HelloWorldController.java
@@ -2,14 +2,15 @@ package com.example.demo.helloworld;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
     @Value("${HELLO:hola}")
     private String greeting;
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return greeting + " Icesi";
+    @GetMapping("/hello/{name}")
+    public String helloWorld(@PathVariable("name") String name) {
+        return greeting + " " + name;
     }
 }
