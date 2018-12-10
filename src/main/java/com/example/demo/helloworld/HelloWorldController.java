e32e74e3f5376ea34e987b7ac8fb4a87f7a8214c
unknown <Invitados@GBENJUMEAV.pslcol.com.co>
2018-12-10 14:41:08 -0500

    Reemplazo hello por hola

--- src/main/java/com/example/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/example/demo/helloworld/HelloWorldController.java
@@ -1,12 +1,15 @@
 package com.example.demo.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    @Value("${HELLO:hola}")
+    private String greeting;
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello Icesi";
+        return greeting + " Icesi";
     }
 }
